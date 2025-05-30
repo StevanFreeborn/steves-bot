7a12f2768d1333f2a43989f0474378eb2b51f806
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2025-05-30 17:27:23 -0500

    tests: add tests for page info

--- src/tests/StevesBot.Webhook.Tests/Unit/YouTubePageInfoTests.cs
+++ src/tests/StevesBot.Webhook.Tests/Unit/YouTubePageInfoTests.cs
@@ -2,5 +2,28 @@ namespace StevesBot.Webhook.Tests.Unit;
 
 public class YouTubePageInfoTests
 {
+  [Fact]
+  public void Constructor_WhenCalled_ItShouldReturnAnInstance()
+  {
+    var result = new YouTubePageInfo();
 
+    result.TotalResults.Should().Be(0);
+    result.ResultsPerPage.Should().Be(0);
+  }
+
+  [Fact]
+  public void Constructor_WhenCalledWithValues_ItShouldReturnAnInstance()
+  {
+    var total = 10;
+    var resultsPerPage = 1;
+
+    var result = new YouTubePageInfo()
+    {
+      TotalResults = total,
+      ResultsPerPage = resultsPerPage,
+    };
+
+    result.TotalResults.Should().Be(total);
+    result.ResultsPerPage.Should().Be(resultsPerPage);
+  }
 }
